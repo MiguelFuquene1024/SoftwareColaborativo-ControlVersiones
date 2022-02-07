b9a600586bf8fd3ca923f0b92c7a28bf6843f7bc
MiguelFuquene1024 <migueplay_10@hotmail.com>
2022-02-07 14:05:51 -0500

    Initial Structure

---
+++ src/main/java/com/softwarecolaborativo/controlversiones/ControlversionesApplication.java
@@ -0,0 +1,13 @@
+package com.softwarecolaborativo.controlversiones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlversionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlversionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softwarecolaborativo/controlversiones/ControlversionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwarecolaborativo.controlversiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlversionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
