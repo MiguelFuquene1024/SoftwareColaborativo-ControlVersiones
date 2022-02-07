84ee740b9140ef5b30789ee3e6ab9b8ad8011232
MiguelFuquene1024 <migueplay_10@hotmail.com>
2022-02-07 14:14:11 -0500

    second change

--- src/main/java/com/softwarecolaborativo/controlversiones/ControlversionesApplication.java
+++ src/main/java/com/softwarecolaborativo/controlversiones/ControlversionesApplication.java
@@ -9,6 +9,7 @@ public class ControlversionesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ControlversionesApplication.class, args);
 		System.out.println("Hello World")
+		System.out.println("Esto es una prueba")
 	}
 
 }
